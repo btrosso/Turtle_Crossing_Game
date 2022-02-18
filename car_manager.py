bd0cd430afb94530789e3500d420d68661975b72
btrosso <51491995+btrosso@users.noreply.github.com>
2022-02-18 00:15:45 -0600

    first major commit
    - got the player turtle, its reset method, and movement
    - started working on the car_manager
    - got a car and i can make it move and i can create multiple cars need to figure out how to space their timing out
    
    Took 1 hour 15 minutes

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,35 @@
+import random
+import time
+from turtle import Turtle
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+# BOUNDARY for cars is y = -220 to y = 220
+
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.parking_lot = []
+
+    def move_cars(self):
+        for car in self.parking_lot:
+            car.forward(STARTING_MOVE_DISTANCE)
 
 
-class CarManager:
-    pass
+    def create_car(self):
+        if len(self.parking_lot) < 1:
+            new_car = Turtle("square")
+            new_car.hideturtle()
+            new_car.pu()
+            new_car.color(random.choice(COLORS))
+            new_car.setheading(180)
+            new_car.shapesize(stretch_len=2, stretch_wid=1)
+            new_car.goto(300, random.randint(-220, 220))
+            new_car.showturtle()
+            self.parking_lot.append(new_car)
+        else:
+            pass
+
+
+
--- main.py
+++ main.py
@@ -4,14 +4,32 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+tim = Player()
+car_manager = CarManager()
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+screen.listen()
+screen.onkey(fun=tim.move, key="Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # TODO: may need to rethink how to create the cars
+    # Detect car crossing left boundary
+    # for car in car_manager.parking_lot:
+    #     if car.xcor() < -280:
+    #         car_manager.reset()
+
+    # Detect crossing finish line
+    if tim.ycor() > 280:
+        tim.reset()
 
 
 
